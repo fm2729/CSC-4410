984e372caa965a296302c3d2c4bbc89852beeea2
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-11 20:40:31 -0500

    Add files via upload

---
+++ HW4-1.py
@@ -0,0 +1,16 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 4 Issue 1###
+# Revision number BEGIN/ 02/09/21
+## Begin Hussein El Sibai
+x = input("Please enter a string: ")
+Please enter a string: Python
+print('You entered',x,'and its type is <class "str">')
+You entered Python and its type is <class "str">
+y = int(input('Please enter an integer:'))
+Please enter an integer:123
+print('You entered',y,'and its type is <class "str">')
+You entered 123 and its type is <class "str">
+# Revision number 2/ 02/09/21
+## End Hussein El Sibai
+# Omega Group/ Ram Valud/ Michael Walker/ project # greenwood321
