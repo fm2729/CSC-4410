5376ac234be380ab6257fd4b67a4ba9d3dd589dd
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-11 20:41:21 -0500

    Add files via upload

---
+++ HW4-3.py
@@ -0,0 +1,13 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 4 Issue 3###
+# Revision number BEGIN/ 02/09/21
+## Begin Hussein El Sibai
+r =1.1
+Area = 3.8013271108436504
+a = Area * r * r
+print("The area of a circle is:",a)
+The area of a circle is: 4.599605804120817
+# Revision number 2/ 02/09/21
+## End Hussein El Sibai
+# Omega Group/ Ram Valud/ Michael Walker/ project # greenwood321
