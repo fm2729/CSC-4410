18d0ed974373007591299810e1abef59c5c67970
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-11 20:40:59 -0500

    Add files via upload

---
+++ HW4-2.py
@@ -0,0 +1,15 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 4 Issue 2###
+# Revision number BEGIN/ 02/09/21
+## Begin Hussein El Sibai
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+Twinkle, twinkle, little star, 
+	How I wonder what you are! 
+		Up above the world so high, 
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+# Revision number 2/ 02/09/21
+## End Hussein El Sibai
+# Omega Group/ Ram Valud/ Michael Walker/ project # greenwood321
