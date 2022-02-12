bf3e43a6e1638997b9f24f1f5021d22d8f752392
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-11 20:42:23 -0500

    Add files via upload

---
+++ HW4-5.py
@@ -0,0 +1,11 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 4 Issue 5###
+# Revision number BEGIN/ 02/09/21
+## Begin Hussein El Sibai
+seq = [ pow(i,4) for i in range(20)]
+print(seq)
+[0, 1, 16, 81, 256, 625, 1296, 2401, 4096, 6561, 10000, 14641, 20736, 28561, 38416, 50625, 65536, 83521, 104976, 130321]
+# Revision number 2/ 02/09/21
+## End Hussein El Sibai
+# Omega Group/ Ram Valud/ Michael Walker/ project # greenwood321
