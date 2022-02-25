37fb6251ccb5b3fd00d5090a2df09a6edbdc1f14
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-25 15:42:08 -0500

    Add files via upload

---
+++ Assignment 6-3.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 6 Issue 3###
+# Revision number BEGIN/ 02/25/22
+## Begin Hussein El Sibai
+##Read a value from the user
+str1 = "Animals"
+str2 = "Badger"
+str3 = "Honey Bee"
+str4 = "Honey Badger"
+print(str1.lower())
+animals
+print(str2.lower())
+badger
+print(str3.lower())
+honey bee
+print(str4.lower())
+honey badger
+print(str1.upper())
+ANIMALS
+print(str2.upper())
+BADGER
+print(str3.upper())
+HONEY BEE
+print(str4.upper())
+HONEY BADGER
+string1 = " Filet Mignon"
+string1.replace(" ", "")
+'FiletMignon'
+string2 = "Brisket "
+string2.replace(" ", "")
+'Brisket'
+string3 = " Cheeseburger "
+string3.replace(" ", "")
+'Cheeseburger'
+text1 = "Becomes"
+result1 = text1.startswith("be")
+print(result1)
+False
+text2 = "becomes"
+result2 = text2.startswith("be")
+print(result2)
+True
+text3 = "BEAR"
+result3 = text3.startswith("be")
+print(result3)
+False
+# Revision number 1/ 02/25/22
+## End Hussein El Sibai
+# Zion Worship Cult/ Ram Vuduku/ Rich Eissen/ project # the Zion Project
+
