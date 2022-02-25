e8fb68431ccb6eebe1160af650f5e0fdb6a4c5d5
fm2729 <90117796+fm2729@users.noreply.github.com>
2022-02-25 15:41:31 -0500

    Add files via upload

---
+++ Assignment 6-2.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+###Assignment 6 Issue 2###
+# Revision number BEGIN/ 02/25/22
+## Begin Hussein El Sibai
+##Read a value from the user
+str1 = "Hussein El Sibai"
+print("The length of the string is:",len(str1))
+The length of the string is: 16
+str2 = "Hussein"
+str3 = "El Sibai"
+str4 = str2 + str3
+print(str4)
+HusseinEl Sibai
+str41 = str2 + '' + str3
+print(str41)
+HusseinEl Sibai
+str42 = str2 + " " + str3
+print(str42)
+Hussein El Sibai
+print("bazinga"[2:6])
+zing
+# Revision number 1/ 02/25/22
+## End Hussein El Sibai
+# Zion Worship Cult/ Ram Vuduku/ Rich Eissen/ project # the Zion Project
